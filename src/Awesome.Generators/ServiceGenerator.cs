cb38d0b0796f284df0f550ddd62c2b13a441c23b
enisn <info@enisnecipoglu.com>
2022-01-15 13:37:28 +0300

    Add blank source generator

---
+++ src/Awesome.Generators/ServiceGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Awesome.Generators;
+
+[Generator]
+public class ServiceGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
