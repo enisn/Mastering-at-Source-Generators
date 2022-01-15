73fb870867074d01fc34bf76305f41f0a6b4ee47
enisn <info@enisnecipoglu.com>
2022-01-15 15:00:01 +0300

    Executing the Source Generator

---
+++ src/Awesome.Api/Todo.cs
@@ -0,0 +1,11 @@
+﻿using Awesome.Generators;
+
+namespace Awesome.Api;
+
+[GenerateService]
+public class Todo
+{
+    public Guid Id { get; set; }
+    public string Content { get; set; }
+    public bool IsCompleted { get; set; }
+}
