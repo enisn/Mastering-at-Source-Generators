8268b4b043ffa9ff2426accef521146d267f02f9
enisn <info@enisnecipoglu.com>
2022-01-15 14:16:28 +0300

    Add default template

---
+++ src/Awesome.Generators/Templates/DefaultTemplateParameters.cs
@@ -0,0 +1,17 @@
+﻿namespace Awesome.Generators.Templates;
+
+public class DefaultTemplateParameters
+{
+    public DefaultTemplateParameters(string typeName, string typeNamespace, string rootNamespace)
+    {
+        ClassName = typeName ?? throw new ArgumentNullException(nameof(typeName));
+
+        Namespace = typeNamespace ?? throw new ArgumentNullException(nameof(typeNamespace));
+
+        PrefferredNamespace = rootNamespace ?? throw new ArgumentNullException(nameof(rootNamespace));
+    }
+
+    public string ClassName { get; set; }
+    public string Namespace { get; set; }
+    public string PrefferredNamespace { get; set; }
+}
