8b87e40ffbe7511ff945d1afcb3c6f706203e2c7
enisn <info@enisnecipoglu.com>
2022-01-15 16:01:12 +0300

    Add a new custom endpoint

---
+++ src/Awesome.Api/Controllers/TodoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Awesome.Api.Controllers;
+
+public partial class TodoController
+{
+    [HttpPut("mark-all-as-completed")]
+    public async Task MarkAllAsCompletedAsync()
+    {
+        var todos = await _repository.GetListAsync();
+
+        foreach (var todo in todos.Where(x => !x.IsCompleted))
+        {
+            await _repository.UpdateAsync(todo.Id, todo);
+        }
+    }
+}
